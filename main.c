bbf27edf4de9457b6b5be781c6c15149a82e8b24
Shelby Smith <galaxybuster@users.noreply.github.com>
2016-01-21 13:53:01 -0700

    bouncing ball program

--- main.c
+++ main.c
@@ -1,12 +1,38 @@
 #include <ncurses.h>
+#include <unistd.h>
 
 int main()
-{	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+{
+	int x = 0, y = 0;
+	int xdir = 1, ydir = 1;
+	int max_x, max_y;
 
-	return 0;
+	initscr();			/* Start curses mode 		  */
+	noecho();
+	curs_set(FALSE);
+
+	// Get the bounds of the console
+	getmaxyx(stdscr, max_y, max_x);
+
+	while (1) {
+		clear();
+
+		mvprintw(y, x, ".");
+		refresh();
+		
+
+		x += xdir;
+		y += ydir;
+
+		if (x < 0 || x > max_x-1) {
+			xdir = -xdir;
+		}
+		if (y < 0 || y > max_y-1) {
+			ydir = -ydir;
+		}
+		
+		usleep(30000);
+	}
+
+	endwin();
 }

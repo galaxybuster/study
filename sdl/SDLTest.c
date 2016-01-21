7bfbc2ef0c96280962be0fafb843b746326269a2
Shelby Smith <galaxybuster@users.noreply.github.com>
2016-01-21 16:27:15 -0700

    let's open a window

--- sdl/SDLTest.c
+++ sdl/SDLTest.c
@@ -1,85 +1,53 @@
+// Example program:
+// Using SDL2 to create an application window
+
+#include "SDL.h"
 #include <stdio.h>
-#include <SDL.h>
 
-#define WIDTH 640
-#define HEIGHT 480
-#define BPP 4
-#define DEPTH 32
+int main(int argc, char* argv[]) {
 
-void setpixel(SDL_Surface *screen, int x, int y, Uint8 r, Uint8 g, Uint8 b)
-{
-    Uint32 *pixmem32;
-    Uint32 colour;  
- 
-    colour = SDL_MapRGB( screen->format, r, g, b );
-  
-    pixmem32 = (Uint32*) screen->pixels  + y + x;
-    *pixmem32 = colour;
+	SDL_Window *window;                    // Declare a pointer
+
+	SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+	
+	// Create an application window with the following settings:
+	window = SDL_CreateWindow(
+		"SDL Test",                  // window title
+		SDL_WINDOWPOS_UNDEFINED,           // initial x position
+		SDL_WINDOWPOS_UNDEFINED,           // initial y position
+		640,                               // width, in pixels
+		480,                               // height, in pixels
+		SDL_WINDOW_SHOWN                // flags - see below
+	);
+
+	// Check that the window was successfully created
+	if (window == NULL) {
+		// In the case that the window could not be made...
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	// The window is open: could enter program loop here (see SDL_PollEvent())
+
+	int end = 0;
+	
+	SDL_Event event;
+	while (!end) {
+		while (SDL_PollEvent(&event)) {
+			// handle your event here
+			switch (event.type) {
+				case SDL_QUIT:
+					end = 1;
+				break;
+			}
+		}
+	}
+
+
+	// Close and destroy the window
+	SDL_DestroyWindow(window);
+
+	// Clean up
+	SDL_Quit();
+	return 0;
 }
-
-
-void DrawScreen(SDL_Surface* screen, int h)
-{ 
-    int x, y, ytimesw;
-  
-    if(SDL_MUSTLOCK(screen)) 
-    {
-        if(SDL_LockSurface(screen) < 0) return;
-    }
-
-    for(y = 0; y < screen->h; y++ ) 
-    {
-        ytimesw = y*screen->pitch/BPP;
-        for( x = 0; x < screen->w; x++ ) 
-        {
-            setpixel(screen, x, ytimesw, (x*x)/256+3*y+h, (y*y)/256+x+h, h);
-        }
-    }
-
-    if(SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
-  
-    SDL_Flip(screen); 
-}
-
-
-int main(int argc, char* argv[])
-{
-    SDL_Surface *screen;
-    SDL_Event event;
-  
-    int keypress = 0;
-    int h=0; 
-  
-    if (SDL_Init(SDL_INIT_VIDEO) < 0 ) return 1;
-   
-    if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_FULLSCREEN|SDL_HWSURFACE)))
-    {
-        SDL_Quit();
-        return 1;
-    }
-  
-    while(!keypress) 
-    {
-         DrawScreen(screen,h++);
-         while(SDL_PollEvent(&event)) 
-         {      
-              switch (event.type) 
-              {
-                  case SDL_QUIT:
-	              keypress = 1;
-	              break;
-                  case SDL_KEYDOWN:
-                       keypress = 1;
-                       break;
-              }
-         }
-    }
-
-    SDL_Quit();
-  
-    return 0;
-}
-
-
-
-
